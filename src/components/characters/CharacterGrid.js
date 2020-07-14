887f449170f1f4564a11104002fed1f19992db16
Cypher <adityasaha444@gmail.com>
2020-07-14 10:31:21 +0530

    character item created

--- src/components/characters/CharacterGrid.js
+++ src/components/characters/CharacterGrid.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import CharacterItem from './CharacterItem';
 
 const CharacterGrid = ({ isLoading, items }) => {
   return isLoading ? (
@@ -6,7 +7,7 @@ const CharacterGrid = ({ isLoading, items }) => {
   ) : (
     <section className='cards'>
       {items.map((item) => (
-        <h1 key={item.char_id}>{item.name}</h1>
+        <CharacterItem key={item.char_id} item={item} />
       ))}
     </section>
   );
