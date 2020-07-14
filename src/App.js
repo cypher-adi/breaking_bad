880d228b2d2ef24ee8b358a628a08de666e20c66
Cypher <adityasaha444@gmail.com>
2020-07-14 10:16:01 +0530

    fetch data using axios and store to items

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Header from './components/ui/Header';
+import axios from 'axios';
 
-function App() {
+const App = () => {
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      const result = await axios(
+        `https://www.breakingbadapi.com/api/characters`
+      );
+
+      console.log(result.data);
+
+      setItems(result.data);
+      setIsLoading(false);
+    };
+
+    fetchItems();
+  }, []);
+
   return (
     <div className='container'>
       <Header />
     </div>
   );
-}
+};
 
 export default App;
