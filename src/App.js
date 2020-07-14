efd4569bcee05e701aac97da86efa978993c72b0
Cypher <adityasaha444@gmail.com>
2020-07-14 09:58:24 +0530

    Header created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Header from './components/ui/Header';
 
 function App() {
   return (
     <div className='container'>
-      <h1>Hello</h1>
+      <Header />
     </div>
   );
 }
