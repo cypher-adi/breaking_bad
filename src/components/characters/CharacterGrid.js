7643e7ca9bd89130659079f5c6e29e78cf27ebd4
Cypher <adityasaha444@gmail.com>
2020-07-14 10:33:43 +0530

    Spinner added

--- src/components/characters/CharacterGrid.js
+++ src/components/characters/CharacterGrid.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import CharacterItem from './CharacterItem';
+import Spinner from '../ui/Spinner';
 
 const CharacterGrid = ({ isLoading, items }) => {
   return isLoading ? (
-    <h1>Loading ...</h1>
+    <Spinner />
   ) : (
     <section className='cards'>
       {items.map((item) => (
