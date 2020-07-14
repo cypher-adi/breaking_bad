042f52644df9e21ee8343485f3650f42ade40d5f
Cypher <adityasaha444@gmail.com>
2020-07-14 10:43:52 +0530

    searchbar functionality added

--- src/App.js
+++ src/App.js
@@ -8,26 +8,27 @@ import './App.css';
 const App = () => {
   const [items, setItems] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [query, setQuery] = useState('');
 
   useEffect(() => {
     const fetchItems = async () => {
-      const result = await axios(
-        `https://www.breakingbadapi.com/api/characters`
-      );
+      setIsLoading(true);
 
-      console.log(result.data);
+      const result = await axios(
+        `https://www.breakingbadapi.com/api/characters?name=${query}`
+      );
 
       setItems(result.data);
       setIsLoading(false);
     };
 
     fetchItems();
-  }, []);
+  }, [query]);
 
   return (
     <div className='container'>
       <Header />
-      <Search />
+      <Search getQuery={(q) => setQuery(q)} />
       <CharacterGrid isLoading={isLoading} items={items} />
     </div>
   );
--- src/components/ui/Search.js
+++ src/components/ui/Search.js
@@ -1,6 +1,13 @@
 import React, { useState } from 'react';
 
-const Search = () => {
+const Search = ({ getQuery }) => {
+  const [text, setText] = useState('');
+
+  const onChange = (q) => {
+    setText(q);
+    getQuery(q);
+  };
+
   return (
     <section className='search'>
       <form>
@@ -8,6 +15,8 @@ const Search = () => {
           type='text'
           className='form-control'
           placeholder='Search characters'
+          value={text}
+          onChange={(e) => onChange(e.target.value)}
           autoFocus
         />
       </form>
